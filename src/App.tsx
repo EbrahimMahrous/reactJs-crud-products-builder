ccb343e624bcf33eab3fd6fa3fd93a0a10a20625
Ibrahim Mahrous <ebraheemalimahrous000@gmail.com>
2025-04-11 16:43:48 +0200

    Error message toggle

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,18 @@ import Button from "./components/UI/Button";
 // ** Data
 import { formInputsList, ProductList } from "./data";
 // ** Hooks
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { IProduct } from "./interfaces";
+import { productValidation } from "./validation";
+import ErrorMessage from "./components/ErrorMessage";
 
 
 
 
 
 export default function App(){
-    // ** State
-    const [product, setProduct] = useState<IProduct>({
-      title: '',
+  const defaultProductObj = {
+    title: '',
       description: '',
       imageURL: '',
       price: '',
@@ -26,21 +27,57 @@ export default function App(){
         name: '',
         imageURL: ''
       }
-    })
-
+  }
+    // ** State
+    const [product, setProduct] = useState<IProduct>(defaultProductObj)
+    const [errors, setErrors] = useState({title: '', description: '', imageURL: '', price: ''})
     const [isOpen, setIsOpen] = useState(false)
 
     // ** Handler
     const openModel = () => {setIsOpen(true)}
     const closeModel = () => {setIsOpen(false)}
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-      const {value, name} = e.target
+    const {value, name} = e.target
+      // ** Set Product
       setProduct({
         ...product,
         [name]: value,
       })
+      // ** Clear Error for UX 
+      setErrors({
+        ...errors,
+        [name]: ''
+      })
     }
-    console.log(product)
+    const onCancel = () => {
+      // ** Clear
+      console.log('cancel')
+      setProduct(defaultProductObj)
+      closeModel()
+    }
+    const submitHandler = (e: FormEvent<HTMLFormElement>): void => {
+      e.preventDefault()
+      const {title, description, imageURL ,price} = product
+      const errors = productValidation(
+        {
+          title, 
+          description, 
+          imageURL, 
+          price
+        })
+      // ** check if any property has a value of "" && check if all properties have a value of ""
+      const hasErrorMsg = Object.values(errors).some((value => value === '')) && Object.values(errors).every((value => value === ''))
+      if(!hasErrorMsg){
+        setErrors(errors)
+        return
+      }
+      console.log('Send Success')
+    }
+
+
+
+
+
 
     // ** Renders
     const renderProductList = ProductList.map((product)=>{
@@ -48,7 +85,7 @@ export default function App(){
     })
 
     const renderFormInputList = formInputsList.map((input => {
-      return <div className="flex flex-col">
+      return <div className="flex flex-col" key={input.id}>
         <label htmlFor= {input.id} className="mb-[2px] text-sm font-medium text-gray-700">{input.lable}</label>
         <Input 
         type= 'text' 
@@ -57,8 +94,11 @@ export default function App(){
         value= {product[input.name]}
         onChange= {onChangeHandler}
         />
+        <ErrorMessage msg={errors[input.name]} />
       </div>
     }))
+
+
     return (
       <main className="container mx-auto sm:p-8 lg:p-16 xl:p-24 2xl:p-28">
         <Button 
@@ -72,11 +112,11 @@ export default function App(){
         </div>
   
         <Modal isOpen= {isOpen} closeModal={closeModel} title="ADD A NEW PRODUCT">
-          <form className="space-y-3">
+          <form className="space-y-3" onSubmit={submitHandler}>
             {renderFormInputList}
             <div className="flex items-center space-x-3">
               <Button className="bg-indigo-400 hover:bg-indigo-500 p-4 w-full rounded-md text-white">Submit</Button>
-              <Button className="bg-red-400 hover:bg-red-500 p-4 w-full rounded-md text-white">Cancel</Button>
+              <Button className="bg-red-400 hover:bg-red-500 p-4 w-full rounded-md text-white" onClick={onCancel}>Cancel</Button>
             </div>
           </form>
         </Modal>
